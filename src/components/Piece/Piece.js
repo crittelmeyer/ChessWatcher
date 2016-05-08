ae03afc6929172453b38e20c72c32a0192c12e7e
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2016-05-07 20:21:26 -0400

    Add Piece component which essentially just wraps the Icon component

---
+++ src/components/Piece/Piece.js
@@ -0,0 +1,17 @@
+import { PropTypes } from 'react'
+import Icon from 'components/Icon'
+
+export const Piece = (props) => (
+  <Icon name={props.name} size={props.size} style={props.style} />
+)
+Piece.propTypes = {
+  name: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+  style: PropTypes.object
+};
+Piece.defaultProps = {
+  name: '',
+  size: 36
+};
+
+export default Piece
